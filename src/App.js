05e0f54045de43fd37e5d4b46b36894d1afa089d
AzatResh <Azat.R.2003@mail.ru>
2023-07-06 23:39:16 +0600

    - header design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import React from 'react';
@@ -12,24 +10,14 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      posts: [
-        {id: 'abc1', name: "JS"},
-        {id: 'abc2', name: "React"},
-        {id: 'abc3', name: "Flutter"},
-      ]
     }
   }
 
-  removePost = (id) =>{
-    this.setState({posts: this.state.posts.filter(post => (post.id!==id))});
-  }
-
   render(){
-    const {posts} = this.state;
-
     return(
-      
-      <Header />
+      <>
+        <Header />
+      </>
     );
   }
 }
