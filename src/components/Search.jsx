d68739bf922e40838bb91c7b9c8994b31b5c6946
AzatResh <Azat.R.2003@mail.ru>
2023-07-09 22:29:45 +0600

    - Search рефакторинг

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,53 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 
-class Search extends React.Component{
+const Search = (props) => {
 
-    state = {
-        search: '',
-        type: 'all'
-    }
+    const {
+        searchMovies = Function.prototype
+    } = props;
 
-    handleKey = (event) => {
+    const [search, setSearch] = useState('');
+    const [type, setType] = useState('all');
+
+    const handleKey = (event) => {
         if(event.key === 'Enter'){
-            this.props.searchMovies(this.state.search, this.state.type);
+            searchMovies(search, type);
         }
     }
 
-    handleRadio = (event) =>{
-        this.setState(() => ({type: event.target.dataset.type}), 
-        ()=>{this.props.searchMovies(this.state.search, this.state.type)})
+    const handleRadio = (event) =>{
+        setType(event.target.dataset.type);
+        searchMovies(search, event.target.dataset.type);
     }
 
-    render(){
-        return(
-            <>
-                <form className="header__form p-3 pb-0" role="search" onSubmit={(e)=>{e.preventDefault()}}>
-                    <input className="form-control me-2" 
-                            type="search" 
-                            placeholder="Search" 
-                            value={this.state.search}
-                            onChange={(e) => {this.setState({search: e.target.value})}}
-                            onKeyDown={this.handleKey}
-                            aria-label="Search" />
-                    <button className="btn btn-outline-success" type="submit" onClick={()=>{this.props.searchMovies(this.state.search, this.state.type)}}>Search</button>
-                </form>
+    return(
+        <>
+            <form className="header__form p-3 pb-0" role="search" onSubmit={(e)=>{e.preventDefault()}}>
+                <input className="form-control me-2" 
+                        type="search" 
+                        placeholder="Search" 
+                        value={search}
+                        onChange={(e) => {setSearch(e.target.value)}}
+                        onKeyDown={handleKey}
+                        aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit" onClick={()=>{searchMovies(search, type)}}>Search</button>
+            </form>
 
-                <div className="p-2 ps-3">
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" onChange={this.handleRadio} data-type="all" checked={this.state.type === 'all'}/>
-                        <label className="form-check-label" for="inlineRadio1">All</label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" onChange={this.handleRadio} data-type="movie" checked={this.state.type === 'movie'}/>
-                        <label className="form-check-label" for="inlineRadio2">Movies</label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" onChange={this.handleRadio} data-type="series" checked={this.state.type === 'series'}/>
-                        <label className="form-check-label" for="inlineRadio3">Series</label>
-                    </div>
+            <div className="p-2 ps-3">
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" onChange={handleRadio} data-type="all" checked={type === 'all'}/>
+                    <label className="form-check-label" for="inlineRadio1">All</label>
                 </div>
-            </>
-        );
-    }
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" onChange={handleRadio} data-type="movie" checked={type === 'movie'}/>
+                    <label className="form-check-label" for="inlineRadio2">Movies</label>
+                </div>
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" onChange={handleRadio} data-type="series" checked={type === 'series'}/>
+                    <label className="form-check-label" for="inlineRadio3">Series</label>
+                </div>
+            </div>
+        </>
+    );
+    
 }
 export {Search};
